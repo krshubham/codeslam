2e572a08aa5ffe1567e623108770cca857c2994f
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-07-03 02:11:44 +0530

    Signup form test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome' });
 });
 router.get('/stylesheets',function(req,res,next){
 	res.send('Not allowed!');
