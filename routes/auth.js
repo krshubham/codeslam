358f1242a28074bee86c530bf400025bd026ea1b
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-07-03 02:12:39 +0530

    Removed unnnecssary code from authjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,7 @@
 var express = require('express');
 var router = express.Router()
 
-function getLoginPage(req,res,next){
-	res.render('login.hbs');
-	next();
-}
-
 function doLogin(req,res){
-	next();
-}
-
-function getSignupPage(req,res,next){
-	res.render('signup.hbs');
 	next();
 }
 
@@ -21,9 +11,7 @@ function doSignup(req,res,next){
 }
 
 //Routes to do the authentication.
-router.get('/signup',getSignupPage);
 router.post('/signup',doSignup);
-router.get('/login',getLoginPage);
 router.post('/login',doLogin);
 
 
