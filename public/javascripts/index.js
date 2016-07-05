0457de64b0b623f715d8a1634f0a92436bc2f8cc
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-07-06 00:16:07 +0530

    Some fade effects added to form

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,56 +1,78 @@
 var TxtRotate = function(el, toRotate, period) {
-			this.toRotate = toRotate;
-			this.el = el;
-			this.loopNum = 0;
-			this.period = parseInt(period, 10) || 2000;
-			this.txt = '';
-			this.tick();
-			this.isDeleting = false;
-		};
+	this.toRotate = toRotate;
+	this.el = el;
+	this.loopNum = 0;
+	this.period = parseInt(period, 10) || 2000;
+	this.txt = '';
+	this.tick();
+	this.isDeleting = false;
+};
 
-		TxtRotate.prototype.tick = function() {
-			var i = this.loopNum % this.toRotate.length;
-			var fullTxt = this.toRotate[i];
+TxtRotate.prototype.tick = function() {
+	var i = this.loopNum % this.toRotate.length;
+	var fullTxt = this.toRotate[i];
 
-			if (this.isDeleting) {
-				this.txt = fullTxt.substring(0, this.txt.length - 1);
-			} else {
-				this.txt = fullTxt.substring(0, this.txt.length + 1);
-			}
+	if (this.isDeleting) {
+		this.txt = fullTxt.substring(0, this.txt.length - 1);
+	} else {
+		this.txt = fullTxt.substring(0, this.txt.length + 1);
+	}
 
-			this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+	this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-			var that = this;
-			var delta = 200 - Math.random() * 100;
+	var that = this;
+	var delta = 200 - Math.random() * 100;
 
-			if (this.isDeleting) { delta /= 2; }
+	if (this.isDeleting) { delta /= 2; }
 
-			if (!this.isDeleting && this.txt === fullTxt) {
-				delta = this.period;
-				this.isDeleting = true;
-			} else if (this.isDeleting && this.txt === '') {
-				this.isDeleting = false;
-				this.loopNum++;
-				delta = 500;
-			}
+	if (!this.isDeleting && this.txt === fullTxt) {
+		delta = this.period;
+		this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === '') {
+		this.isDeleting = false;
+		this.loopNum++;
+		delta = 500;
+	}
 
-			setTimeout(function() {
-				that.tick();
-			}, delta);
-		};
+	setTimeout(function() {
+		that.tick();
+	}, delta);
+};
 
-		window.onload = function() {
-			var elements = document.getElementsByClassName('txt-rotate');
-			for (var i=0; i<elements.length; i++) {
-				var toRotate = elements[i].getAttribute('data-rotate');
-				var period = elements[i].getAttribute('data-period');
-				if (toRotate) {
-					new TxtRotate(elements[i], JSON.parse(toRotate), period);
-				}
-			}
+window.onload = function() {
+	var elements = document.getElementsByClassName('txt-rotate');
+	for (var i=0; i<elements.length; i++) {
+		var toRotate = elements[i].getAttribute('data-rotate');
+		var period = elements[i].getAttribute('data-period');
+		if (toRotate) {
+			new TxtRotate(elements[i], JSON.parse(toRotate), period);
+		}
+	}
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
+
+
+//Signup and login form actions on button clicks.
+function showSignupForm(){
+	$('.fa-close').fadeIn();
+	$('#login-form').slideUp();
+	$('#signup-form').slideDown();
+} 
+function showLoginForm(){
+	$('.fa-close').fadeIn();
+	$('#signup-form').slideUp();
+	$('#login-form').slideDown();
+}
+function closeForm(){
+	$('#login-form').fadeOut();
+	$('#signup-form').fadeOut();
+	$('.fa-close').fadeOut();
+}
+
+$('#btn_signup').click(showSignupForm);
+$('#btn_login').click(showLoginForm);
+$('#closeForm').click(closeForm);
