07b7a87a9c50645cca2581a34d547ead470fdd27
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-07-12 00:33:53 +0530

    Testing some xss validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var assert = require('assert');
+var xss = require('xss');
 function doLogin(req, res,next){
 	next();
 }
 
 function doSignup(req, res, next) {
-	var name = req.body.signup_name;
-	var email = req.body.signup_email;
-	var password = req.body.signup_password;
-	var confirm_password = req.body.c_pwd;
+	var name = xss(req.body.signup_name);
+	var email = xss(req.body.signup_email);
+	var password = xss(req.body.signup_pwd);
+	var confirm_password = xss(req.body.c_pwd);
+
 	console.log(name);
 	console.log(email);
 	console.log(password);
