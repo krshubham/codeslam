a9565cefecad212089a16c3db596ba9835697209
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-08-11 22:49:54 +0530

    Signup complet

--- routes/auth.js
+++ routes/auth.js
@@ -8,21 +8,63 @@ var url = 'mongodb://localhost:27017/slam'
 var db = require('./db');
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
+var users = null;
+
+function validateUserDetails(name,email,password,cp){
+	assert.deepEqual(cp,password);
+	if(password.length>=6 && password.length<=32 && validate.isEmail(email) && typeof(name) === 'string' ){
+		return;
+	}
+	else{
+		var err = new Error('Error in Validating the user');
+		throw err;
+	}
+}
 
 function doSignup(req,res,next){
+	users = db.get().collection('users');
 	var name = xss(req.body.signup_name),
-		email = xss(req.body.signup_email),
-		password = xss(req.body.Signup_pwd),
-		confirm = xss(req.body.c_pwd);
-		try{
-			assert.deepEqual(confirm,password);
-			db.get()
-		}
+	email = xss(req.body.signup_email),
+	password = xss(req.body.signup_pwd),
+	confirm = xss(req.body.c_pwd);
+	
+	try{
+		validateUserDetails(name,email,password,confirm);
+		bcrypt.hash(password, saltRounds, function(err, hash) {
+			assert.equal(err,null);
+			var user = {
+				name: name,
+				email: email,
+				password: hash
+			}
+			users.findOne({email: email},function(err,user){
+				assert.equal(err,null);
+				if(user){
+					var vm = {
+						title: 'Welcome',
+						success: false
+					};
+					return res.render(index,vm);
+				}
+				else{
+					users.insertOne(user,function(err,done){
+						assert.equal(err,null);
+						console.log(done);
+					});
+				}
+			});
+
+		});
+	}
+	catch(err){
+		console.log('Hello' + err);
+	}
 }
+
 
 //Routes to do the authentication.
 router.post('/signup',doSignup);
-router.post('/login',doLogin);
+//router.post('/login',doLogin);
 
 
 module.exports = router;
