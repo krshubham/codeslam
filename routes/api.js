72d2a05b4bddec24dbd576d58fab815f2ba64f44
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-09-24 01:33:38 +0530

    pushing for server mail check

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,13 @@ var db = require('./db');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
+var cuid = require('cuid');
 var secret = 'g@@k@911';
-
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport('smtps://kumar.shubham92411%40gmail.com:mkj1970@#@smtp.gmail.com');
 
 //The login handler for the api
-router.post('/login',function(req,res,next){
+router.post('/login', function (req, res, next) {
 	//checking if the code is executing or not
 	console.log('received the request for login');
 
@@ -26,45 +28,45 @@ router.post('/login',function(req,res,next){
 	var person = {
 		email: email,
 	}
-
 	//Finding the user from the db.
-	users.findOne(person,function(err,user){
-		try{
+
+	users.findOne(person, function (err, user) {
+		try {
 			//check if there is no error
-			assert.equal(err,null);
+			assert.equal(err, null);
 			//check if the user is found
 			console.log(user);
-			if(!user){
+			if (!user) {
 				//user not found
 				return res.json({
 					success: false,
 					message: 'Something is wrong Please try again.'
 				});
 			}
-			else{
+			else {
 				//user is found
 				//Now we compare the password of the user
-				bcrypt.compare(password,user.password,function(err,result){
-					assert.equal(err,null);
-					if(!result){
+				bcrypt.compare(password, user.password, function (err, result) {
+					assert.equal(err, null);
+					if (!result) {
 						//Result is not there, assword wrong
 						console.log('Error in password');
 						//send 401 status code saying Unauthorized
 						return res.json({
 							success: false,
 							message: 'Something is wrong! Please try again.'
-						});	
+						});
 					}
-					else{
+					else {
 						//everything is fine,
 						//Give the token to the user.
 						var item = {
 							_id: user._id,
 							email: user.email,
-							name: user.name,							 
+							name: user.name,
 						};
 						//sign the token to the user
-						var token = jwt.sign(item,secret,{
+						var token = jwt.sign(item, secret, {
 							expiresIn: 86400
 						});
 						//send the correct response to the user
@@ -76,7 +78,7 @@ router.post('/login',function(req,res,next){
 				});
 			}
 		}
-		catch(err){
+		catch (err) {
 			//catch all the errors and set success flag to false
 			//send a forbidden status code
 			res.status(403).json({
@@ -86,50 +88,67 @@ router.post('/login',function(req,res,next){
 		}
 	});
 });
-
-function Signup(req,res,next){
+//The signup function
+function Signup(req, res, next) {
 	console.log('Signup request received');
-	users = db.get().collection('users');
+	users = db.get().collection('tempUsers');
 	var name = xss(req.body.name),
-	email = xss(req.body.email),
-	password = xss(req.body.password),
-	confirm_password = xss(req.body.cp);
-	try{
+		email = xss(req.body.email),
+		password = xss(req.body.password),
+		confirm_password = xss(req.body.cp);
+	try {
 		//if js is not enabled, verify here.
-		assert.deepEqual(password,confirm_password);
+		assert.deepEqual(password, confirm_password);
 		//make sure that the user does not exist already,
 		var user = {
 			email: email
 		}
-		users.findOne(user,function(err,user){
-			assert.equal(err,null);
+		users.findOne(user, function (err, user) {
+			assert.equal(err, null);
 			console.log('checking the user');
-			if(user){//the user exists already
+			if (user) {//the user exists already
 				return res.json({
 					success: false,
 					message: 'User already exists'
 				});
 			}
-			else{
-				bcrypt.hash(password,saltRounds,function(err,hash){
+			else {
+				link = cuid();
+				var mailOptions = {
+					from: '"CodeslamAdmin" <admin@codeslam.com>', // sender address
+					to: email, // list of receivers
+					subject: 'Confirm your Email', // Subject line
+					text: 'Hey there, We recently got a login request for this email. Click on the link below to' +
+					'verify',
+					html: '<a href="http://localhost/verify/' + link + '">Click Here</a>' // html body
+				};
+				console.log(mailOptions.html);
+				bcrypt.hash(password, saltRounds, function (err, hash) {
 					var user = {
 						name: name,
 						email: email,
 						password: hash,
+						v_link: link
 					};
-					users.insertOne(user,function(err,done){
-						assert.equal(err,null);
+					users.insertOne(user, function (err, done) {
+						assert.equal(err, null);
 						console.log('user inserted');
 						return res.json({
 							success: true,
 							message: 'user successfully created'
 						});
 					});
-				});		
+				});
+				transporter.sendMail(mailOptions, function (error, info) {
+					if (error) {
+						return console.log(error);
+					}
+					console.log('Message sent: ' + info.response);
+				});
 			}
 		});
 	}
-	catch(err){
+	catch (err) {
 		console.log(err.message);
 	}
 }
@@ -167,6 +186,6 @@ function Signup(req,res,next){
 
 
 
- 
-router.post('/signup',Signup);
+
+router.post('/signup', Signup);
 module.exports = router;
