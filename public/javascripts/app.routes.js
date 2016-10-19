ec3acf59da7caa9b65958626f658e26a6aaf1f9f
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-10-19 16:00:00 +0530

    Working on the interceptor

--- public/javascripts/app.routes.js
+++ public/javascripts/app.routes.js
@@ -81,11 +81,18 @@ app.config(function ($routeProvider, $locationProvider) {
 })
 	.run(["$rootScope", "$route", "Auth", "$location", function ($rootScope, Auth, $location, $route) {
 		$rootScope.$on("$locationChangeStart", function (event, current, previous, rejection) {
-			if($route.$$path.indexOf('/user/')){
-				console.log('Authenticated route')
-			}
-			else{
-				console.log('unauthenticated route');
-			}
+			console.log(rejection);
+
+			// if ($route.$$path.indexOf('/user/') !== -1) {
+			// 	if ($location.isLoggedIn()) {
+			// 		console.log('You have the access');
+			// 	}
+			// 	else {
+			// 		console.log("you dont have the access");
+			// 	}
+			// }
+			// else {
+			// 	console.log('unauthenticated route');
+			// }
 		});
 	}]);
