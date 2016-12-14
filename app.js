2edefaaaf27cf378bb415b30563a946f2e7a3636
Kumar Shubham <kumar.shubham@wingify.com>
2016-12-14 17:16:11 +0530

    Removed some faaltu files

--- app.js
+++ app.js
@@ -67,25 +67,25 @@ app.use(function (req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.status(500).json({
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-// app.use(function (err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.status(500).json({
-//     message: err.message,
-//     error: {}
+// if (app.get('env') === 'development') {
+//   app.use(function (err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.status(500).json({
+//       message: err.message,
+//       error: err
+//     });
 //   });
-// });
+// }
+
+production error handler
+no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.status(500).json({
+    message: err.message,
+    error: {}
+  });
+});
 
 
 module.exports = app;
